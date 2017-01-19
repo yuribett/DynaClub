9356b0dd7a1a74c31f62cf9a03c9f4725bbf12af
Yuri Bett <yuribett@gmail.com>
2017-01-19 12:43:29 -0200

    API token verify

--- server/app/routes/auth.js
+++ server/app/routes/auth.js
@@ -2,6 +2,5 @@ module.exports = function(app) {
 
     var api = app.api.auth;
     app.post('/auth', api.authUser);
-    //TODO
-    //app.use('/*', api.verifyToken);
+    app.use('/*', api.verifyToken);
 };
