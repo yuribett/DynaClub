36ad3952196528c0e344f3cdd1b92db605c81ce9
Yuri Bett <yuribett@gmail.com>
2017-01-20 11:41:38 -0200

    Dynamix MongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')('mongodb://localhost/dynaclub');
+require('./config/database')('mongodb://mongodb.dynamix.com.br:27017/dynaclub');
 
 http.createServer(app)
 .listen(3000, function() {
