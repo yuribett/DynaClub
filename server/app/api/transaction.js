923772dcfb6d65a66103dd9df24e8dbecbf3d4bb
Anderson de Amorim <anderson@dynamix.com.br>
2017-01-23 13:49:33 -0200

    Transaction FIX(Again)

--- server/app/api/transaction.js
+++ server/app/api/transaction.js
@@ -2,66 +2,52 @@ var mongoose = require('mongoose');
 
 module.exports = function(app) {
 
-	var api = {};
+    var api = {};
 
-	var model = mongoose.model('Transaction');
+    var model = mongoose.model('Transaction');
 
-/*
+
     api.listByUser = function(req, res) {
-		let	user = req.param.user;
-		console.log(user);
-       model.find({ $or : [ {'to' : user}, {'from' : user}]})
-	   	.populate('to from')
-		.then(function(transactions) {
-			res.json(transactions);
-		}, function(error) {
-			console.log(error);
-			res.sendStatus(500);
-		});
+        let user = req.params.user;
+        model.find({ $or: [{ 'to': user }, { 'from': user }] })
+            .populate('to from')
+            .then(function(transactions) {
+                res.json(transactions);
+            }, function(error) {
+                console.log(error);
+                res.sendStatus(500);
+            });
 
-	};*/
+    };
 
-	api.listByUser = function(req, res) {
-		//let	user = req.param.user;
-       model.find()
-	   	.populate('to from')
-		.then(function(transactions) {
-			res.json(transactions);
-		}, function(error) {
-			console.log(error);
-			res.sendStatus(500);
-		});
 
-	};
+    api.insert = function(req, res) {
+        model.create(req.body)
+            .then(function(transaction) {
+                res.json(transaction);
+            }, function(error) {
+                console.log('cannot insert transaction');
+                console.log(error);
+                res.sendStatus(500);
+            });
+    };
 
-	api.insert = function(req, res) {
-		model.create(req.body)
-		.then(function(transaction) {
-			res.json(transaction);
-		}, function(error) {
-			console.log('cannot insert transaction');
-			console.log(error);
-			res.sendStatus(500);
-		});
-	};
+    /*
+        api.findById = function(req, res) {
+    		
+    	};
 
-/*
-    api.findById = function(req, res) {
-		
-	};
+    	api.insert = function(req, res) {
+    		
+    	};
 
-	api.insert = function(req, res) {
-		
-	};
+        api.update = function(req, res) {
+    		
+    	};
 
-    api.update = function(req, res) {
-		
-	};
-
-    api.delete = function(req, res) {
-		
-	};
-*/
-	return api;
+        api.delete = function(req, res) {
+    		
+    	};
+    */
+    return api;
 };
-
