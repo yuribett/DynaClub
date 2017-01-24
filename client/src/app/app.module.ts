745c5993ef8fe9b3d9221b07cb6de6ceb590a8f2
Yuri Bett <yuribett@gmail.com>
2017-01-24 13:16:02 -0200

    Fixing http auth with token

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, XHRBackend } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
 import { DashboardModule } from './dashboard/dashboard.module';
 
 //Components
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin/admin.component';
+import { ProfileComponent } from './settings/profile/profile.component';
 
 //Services
 import { AuthService } from './auth/auth.service';
@@ -18,9 +19,9 @@ import { UserService } from './user/user.service';
 //Other
 import { routing } from './app.routes';
 import 'rxjs/add/operator/map';
-import { ExtendedXHRBackend } from './auth/extended.xhr.backend';
+//import { ExtendedXHRBackend } from './auth/extended.xhr.backend';
 import { LoggedInGuard } from './auth/logged.in.guard';
-import { ProfileComponent } from './settings/profile/profile.component';
+import { HttpService } from './auth/http.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,9 @@ import { ProfileComponent } from './settings/profile/profile.component';
     routing,
     DashboardModule
   ],
-  providers: [{ provide: XHRBackend, useClass: ExtendedXHRBackend },
+  providers: [
+    //{ provide: XHRBackend, useClass: ExtendedXHRBackend },
+    { provide: Http, useClass: HttpService },
     LoggedInGuard,
     AuthService,
     UserService],
