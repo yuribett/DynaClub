8b6398af047678d6df471300248d42f6b62049c6
Yuri Bett <yuribett@gmail.com>
2017-01-29 15:19:32 -0200

    Mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')('mongodb://dynamix:dynamix@ds117839.mlab.com:17839/dynaclub');
+require('./config/database')('mongodb://localhost/dynaclub');
 
 http.createServer(app)
 .listen(3000, function() {
