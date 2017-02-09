f89fe8c5ce5cbf5f01ad5fa3eb7ea7c67af16a87
Yuri Bett <yuribett@gmail.com>
2017-02-08 23:39:26 -0200

    Adding production api path

--- client/src/app/app.globals.ts
+++ client/src/app/app.globals.ts
@@ -1,5 +1,7 @@
+import { environment } from '../environments/environment';
+
 export class Globals {
-	public static API_URL = 'http://localhost:3000/api';
+	public static API_URL = environment.api;
 	public static LOCAL_TOKEN = 'dynaclub-token';
 	public static LOCAL_USER = 'dynaclub-user';
 	public static CURRENT_TEAM = 'dynaclub-team';
