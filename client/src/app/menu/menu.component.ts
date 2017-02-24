210b9241b1db7bcdbde0719a415b5669f41b4aa0
Anderson de Amorim <anderson@dynamix.com.br>
2017-02-24 12:20:32 -0300

    Menu improvements

--- client/src/app/menu/menu.component.ts
+++ client/src/app/menu/menu.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { OnDestroy, Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { AppService } from '../app.service';
 import { User } from '../shared/models/user';
 import { UserService } from '../shared/services/user.service';
 import { Globals } from '../app.globals';
 import { Team } from '../shared/models/team';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
 	selector: 'app-menu',
 	templateUrl: './menu.component.html',
 	styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
 
 	user: User = new User();
 	currentTeam: Team;
+	_subCurrentTeam: Subscription;
 
 	constructor(private auth: AuthService, private appService: AppService, private userService: UserService) {
 		var _self = this;
@@ -33,9 +35,13 @@ export class MenuComponent implements OnInit {
 		this.appService.getUser().subscribe((loggedUser: User) => {
 			_self.user = loggedUser;
 		});
-		this.appService.getCurrentTeam().subscribe((team: Team) => {
+		this._subCurrentTeam = this.appService.getCurrentTeam().subscribe((team: Team) => {
 			_self.currentTeam = team;
 		});
+	}
+
+	ngOnDestroy() {
+		this._subCurrentTeam.unsubscribe();
 	}
 
 	logout(e) {
