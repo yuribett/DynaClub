d3404200440c892af723ca667752ea1f68ae1609
Yuri Bett <yuribett@gmail.com>
2017-02-27 15:32:40 -0300

    Mailer - dependencies and scaffold

--- server/app/services/memcached.js
+++ server/app/services/memcached.js
@@ -2,11 +2,11 @@ let memcached = require('memcached');
 
 module.exports = () => createMencachedClient;
 
-function createMencachedClient(){
-  let client = new memcached('localhost:11211', {
-      retries: 10,
-      retry: 10000,
-      remove: true
-  });
-  return client;
+function createMencachedClient() {
+	let client = new memcached('localhost:11211', {
+		retries: 10,
+		retry: 10000,
+		remove: true
+	});
+	return client;
 }
