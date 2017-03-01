88bec9bab37cec7365147344ee1b3798778109f5
Yuri Bett <yuribett@gmail.com>
2017-03-01 06:57:47 -0300

    Mailer - Dependencies and initial scaffold

---
+++ server/app/services/mailer/templates/test.js
@@ -0,0 +1,21 @@
+module.exports = () => {
+
+    function getTemplate() {
+        return
+            `
+            <!DOCTYPE html>
+            <html>
+            <body>
+            
+            <h1>What a shame!</h1>
+
+            <p>Voc&ecirc; está recebendo este email pois não realizou nenhuma doação no DynaClub na última semana.</p>
+
+            <h3>DOAR AGORA</h3>
+
+            </body>
+            </html>
+            `;
+    }
+    
+}
