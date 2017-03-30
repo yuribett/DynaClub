6845e748c22a04e4a8b7856cf7d1eef3b42454e1
Anderson de Amorim <anderson@dynamix.com.br>
2017-03-30 11:57:17 -0300

    Updating to Angular CLI 1.0.0 and Angular 4.0.0 (HotFix)

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server: 'http://localhost:3000/'
+  server: window.location.protocol + '//' + window.location.host + '/' //thanks to schleuss
 };
