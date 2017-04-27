5e93d7aee37449eb7b4b1ec215de883f10ce1417
Yuri Bett <yuribett@gmail.com>
2017-04-27 11:32:54 -0300

    Lint fixes

--- client/src/app/app.globals.ts
+++ client/src/app/app.globals.ts
@@ -1,10 +1,10 @@
 import { environment } from '../environments/environment';
 
 export class Globals {
-	public static API_URL = environment.server+'api';
-	public static SOCKET_IO_URL = environment.server;
-	public static LOCAL_TOKEN = 'dynaclub-token';
-	public static LOCAL_USER = 'dynaclub-user';
-	public static CURRENT_TEAM = 'dynaclub-team';
-	public static USER_LOCAL_STORAGE = 'dynaclub-use-local-storage';
+  public static API_URL = environment.server + 'api';
+  public static SOCKET_IO_URL = environment.server;
+  public static LOCAL_TOKEN = 'dynaclub-token';
+  public static LOCAL_USER = 'dynaclub-user';
+  public static CURRENT_TEAM = 'dynaclub-team';
+  public static USER_LOCAL_STORAGE = 'dynaclub-use-local-storage';
 }
